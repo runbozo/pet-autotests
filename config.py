00e9c4acd69722bac72dba3eced577455e2667e6
Alexander Belozerov <asbelozerov@sbercloud.ru>
2022-05-18 18:22:41 +0300

    add readme

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 from dynaconf import Dynaconf
 
-DEFAULT_ENV = "dev"  # [dev|prod]
+DEFAULT_ENV = "dev"  # dev only
 
 settings = Dynaconf(
     env=DEFAULT_ENV,
     settings_files=[
         "./dev_settings.toml",
-        "./prod_settings.toml",
     ],
     environments=True,
     merge_enabled=True,
